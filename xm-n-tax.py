75270aed97f1cd0a639b216968ca63aadbcb8d27
YE <yyybiz@163.com>
2018-08-21 10:09:21 +0800

    bug修复

--- xm-n-tax.py
+++ xm-n-tax.py
@@ -2,13 +2,27 @@ import requests, time, re, csv, random
 from urllib.parse import urljoin
 from lxml import html
 
-start_url = "http://12366ww.xm-n-tax.gov.cn:8091/xmgsww/WslyBLH_findPageZx.do?r=0.6327293831471326&type=2&cxm=&bt=&sj_djl=undefined&lx=undefined"
+start_url = "http://12366ww.xm-n-tax.gov.cn:8091/xmgsww/WslyBLH_findPageZx.do?r=0.13276405784518075&type=2&cxm=&bt=&sj_djl=undefined&lx=undefined"
 detail_url = "http://12366ww.xm-n-tax.gov.cn:8091/xmgsww/WslyBLH_wslyXq.do?initbh={}"
 
 
 data_info = []
 headers = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36"
+    "Accept":"*/*",
+    "Accept-Encoding":"gzip, deflate",
+    "Accept-Language":"zh-CN,zh;q=0.9,zh-TW;q=0.8,en-US;q=0.7,en;q=0.6",
+    "Connection":"keep-alive",
+    "Content-Length":6,
+    "Content-Type":"application/x-www-form-urlencoded;utf-8",
+    "Cookie":"JSESSIONID=zqlaVf91QS5UN-7bE2jZ8Q789woIdFBYxwVhzH6FwKB0qQ00xmUU!1956117633",
+    "Host":"12366ww.xm-n-tax.gov.cn:8091",
+    "Origin":"http://12366ww.xm-n-tax.gov.cn:8091",
+    "Referer":"http://12366ww.xm-n-tax.gov.cn:8091/xmgsww/WslyBLH_tozxzx.do?type=2",
+    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
+    "X-Requested-With":"XMLHttpRequest",
+}
+headers2 = {
+"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
 }
 def save(filename):
     file = open(filename, 'w', encoding="utf-8", newline="")
@@ -19,7 +33,7 @@ def save(filename):
 
 def get_detail(url):
     try:
-        res = requests.get(url, headers= headers)
+        res = requests.get(url, headers= headers2, timeout=5)
         if res.status_code == 200:
             con = res.content
             res.encoding = "utf-8"
@@ -34,7 +48,6 @@ def get_detail(url):
 
             content = soup.xpath("//tr[5]/td[@class='g_t_input']")
             content = content[0].xpath("string(.)") if content else "暂无"
-
 
             push_time = soup.xpath("//tr[3]/td[@class='g_t_input'][2]/text()")
             push_time = push_time[0].strip() if push_time else "暂无"
@@ -55,7 +68,7 @@ def get_detail(url):
 def get_list(page):
     try:
         res = requests.post(start_url,data={'page':page}, headers=headers, timeout=5)
-        if res == 200:
+        if res.status_code == 200:
             for item in res.json()['result']['data']:
                 get_detail(detail_url.format(item['initbh']))
                 time.sleep(random.randint(5,20))
